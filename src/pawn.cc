6debbd76a935049909e404f17ba4d7c58eb820a3
Faseeh Irfan <43595947+faseehirfan@users.noreply.github.com>
2024-07-28 22:21:09 +0000

    added pawn class

---
+++ src/pawn.cc
@@ -0,0 +1,22 @@
+#include "pawn.h"
+
+using namespace std;
+
+Pawn::Pawn(Color c, Square *pos): Piece{PieceType::Pawn, c, 1, pos}, canMove2{true} {}
+
+vector<Move> Pawn::getMoves(const Board& b) const { 
+    vector<Move> moves;
+    if (!pos) return moves;
+
+    int currentRow = pos->getRow();
+    int currentCol = pos->getCol();
+
+    if (color == Color::White){
+        if (currentRow == 6 && !board->getSquare(currentRow, 5).getPiece() && !board->getSquare(currentRow, 4).getPiece() ){
+          moves.push_back(Move{MoveType::Regular, PieceType::NoPiece,
+                               PieceType::Pawn, Color::White, Color::White,
+                               currentRow, currentCol, 4, currentCol});
+        }
+    }
+    return moves;
+}
---
+++ src/pawn.h
@@ -0,0 +1,16 @@
+#ifndef PAWN_H
+#define PAWN_H
+
+#include "piece.h"
+#include <vector>
+
+class Pawn : public Piece {
+  private:
+    bool canMove2;
+
+  public:
+    Pawn(Color c, Square *pos);
+    vector<Move> getMoves(const Board& b) const override;
+};
+
+#endif
