2ff5590741cd503c42e81828904eaca7889e0f29
Rawsab Said <rawsab04@gmail.com>
2024-07-28 22:59:52 +0000

    change textview for testing

--- src/textView.cc
+++ src/textView.cc
@@ -11,14 +11,14 @@ void TextView::print() {
     // Print the board
     cout << " ,-----------------," << endl;
     for (int i = 0; i < 8; ++i) {
-        cout << 8 - i << "| ";
+        cout << i << "| ";
         for (int j = 0; j < 8; ++j) {
             cout << board[i][j] << ' ';
         }
         cout << "|" << endl;
     }
     cout << " '-----------------' " << endl;
-    cout << "   a b c d e f g h" << endl;
+    cout << "   0 1 2 3 4 5 6 7" << endl;
 }
 
 TextView::~TextView() {
