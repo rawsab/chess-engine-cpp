cb4f813984d2365d0bf534f8bdb809c3c4921438
Rawsab Said <rawsab04@gmail.com>
2024-07-28 21:52:55 +0000

    update board design

--- src/textView.cc
+++ src/textView.cc
@@ -9,15 +9,16 @@ TextView::TextView() : View() {
 
 void TextView::print() {
     // Print the board
-    cout << "  a b c d e f g h" << endl;
+    cout << " ,-----------------," << endl;
     for (int i = 0; i < 8; ++i) {
-        cout << 8 - i << " ";
+        cout << 8 - i << "| ";
         for (int j = 0; j < 8; ++j) {
             cout << board[i][j] << ' ';
         }
-        cout << 8 - i << endl;
+        cout << "|" << endl;
     }
-    cout << "  a b c d e f g h" << endl;
+    cout << " '-----------------' " << endl;
+    cout << "   a b c d e f g h" << endl;
 }
 
 TextView::~TextView() {
