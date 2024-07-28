24a216a26692e854f30520b965e9e4ad6f9efb50
mkachroo <mkachroo@uwaterloo.ca>
2024-07-28 17:40:55 -0400

    updated

--- src/textView.cc
+++ src/textView.cc
@@ -7,17 +7,19 @@ TextView::TextView() : View() {
     // Constructor
 }
 
-void TextView::print() {
+ostream &operator<<(std::ostream &out, const TextDisplay &td)
+{
     // Print the board
     cout << "  a b c d e f g h" << endl;
     for (int i = 0; i < 8; ++i) {
         cout << 8 - i << " ";
         for (int j = 0; j < 8; ++j) {
-            cout << board[i][j] << ' ';
+            cout << td.board[i][j] << ' ';
         }
         cout << 8 - i << endl;
     }
     cout << "  a b c d e f g h" << endl;
+    return out;
 }
 
 TextView::~TextView() {
