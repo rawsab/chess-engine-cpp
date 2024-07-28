c10313f4894170ea1ff54f8d1b9f34622fdb094a
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-07-28 12:09:55 -0400

    add getters/setters to piece.h

--- src/piece.h
+++ src/piece.h
@@ -13,17 +13,21 @@ using namespace std;
 class Piece {
  private:
  protected:
+  int points;
   PieceType type;
   Color color;
   vector<Move> moves;
 
  public:
-  virtual vector<Move> getMoves() = 0;
+  Piece(PieceType t, Color c);
+  virtual vector<Move> getMoves() const = 0;
   PieceType getType();
   void move(Move m);
   bool isValidMove(Move m, const Board& b);
   bool canCheck(const Board& b);
-  int getValue();
+  int getPoints() const;
+  Color getColour() const;
+  PieceType getType() const;
   virtual ~Piece();
 };
 
