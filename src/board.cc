f8d82c55657f2bd41db12b7c64f20b65ce668765
Rawsab Said <rawsab04@gmail.com>
2024-07-29 19:21:31 +0000

    fix

--- src/board.cc
+++ src/board.cc
@@ -124,12 +124,14 @@ void Board::move(Move m) {
 
 void Board::undoMove() {
 
-    // Square* src = &board[m.move.nr][m.move.nc];
-    // Square* dst = &board[m.move.r][m.move.c];
-    // Piece* p = src->getPiece();
-    // dst->updateSquare(p);
-    // src->updateSquare(m.captured);
-    // p->updateSquare(dst);
+    MoveHistory m = this->popLastMove();
+
+    Square* src = &board[m.move.nr][m.move.nc];
+    Square* dst = &board[m.move.r][m.move.c];
+    Piece* p = src->getPiece();
+    dst->updateSquare(p);
+    src->updateSquare(m.captured);
+    p->updateSquare(dst);
 
 }
 
