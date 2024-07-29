3b2d99669716d9b24483c16b541763046ccda22b
Faseeh Irfan <43595947+faseehirfan@users.noreply.github.com>
2024-07-29 03:51:21 +0000

    remove no colours with nullptr checks

--- src/pawn.cc
+++ src/pawn.cc
@@ -38,16 +38,16 @@ bool Pawn::canMove(int newRow, int newCol) const {
     if (getColor() == Color::White) {
       if (newCol == col + 1 && newRow == row + 1 && newRow < 8 && newCol < 8 &&
           (board->getSquare(newRow, newCol).getPiece()->getColor() == Color::Black ||
-           (enpassant && board->getSquare(newRow, newCol).getPiece()->getColor() == Color::NoColor))) {
+           (enpassant && board->getSquare(newRow, newCol).getPiece() == nullptr))) {
         return true;
       } else if (newCol == col && newRow == row - 1 && newRow >= 0 &&
-                 board->getSquare(newRow, newCol).getPiece()->getColor() == Color::NoColor) {
+                 board->getSquare(newRow, newCol).getPiece() == nullptr) {
         return true;
       } else if (newCol == col - 1 && newRow == row + 1 && newCol >= 0 &&
                  newRow < 8 &&
                  (board->getSquare(newRow, newCol).getPiece()->getColor() == Color::Black ||
                   (enpassant &&
-                   board->getSquare(newRow, newCol).getPiece()->getColor() == Color::NoColor))) {
+                   board->getSquare(newRow, newCol).getPiece() == nullptr))) {
         return true;
       } else if (newCol == col && newRow == row - 2 && canMove2 &&
                  !board->getSquare(newRow, newCol).getPiece() &&
@@ -58,7 +58,7 @@ bool Pawn::canMove(int newRow, int newCol) const {
       if (newCol == col - 1 && newRow == row - 1 && newRow >= 0 &&
           newCol >= 0 &&
           (board->getSquare(newRow, newCol).getPiece()->getColor() == Color::White ||
-           (enpassant && board->getSquare(newRow, newCol).getPiece()->getColor() == Color::NoColor))) {
+           (enpassant && board->getSquare(newRow, newCol).getPiece() == nullptr))) {
         return true;
       } else if (newCol == col && newRow == row + 1 && newRow < 8 && board->getSquare(newRow,newCol).getPiece() == nullptr) {
         return true;
@@ -66,7 +66,7 @@ bool Pawn::canMove(int newRow, int newCol) const {
                  newRow >= 0 &&
                  (board->getSquare(newRow, newCol).getPiece()->getColor() == Color::White ||
                   (enpassant &&
-                   board->getSquare(newRow, newCol).getPiece()->getColor() == Color::NoColor))) {
+                   board->getSquare(newRow, newCol).getPiece() == nullptr))) {
         return true;
       } else if (newCol == col && newRow == row + 2 && newRow < 8 && canMove2 &&
                  !board->getSquare(newRow, newCol).getPiece() &&
@@ -77,3 +77,6 @@ bool Pawn::canMove(int newRow, int newCol) const {
   }
   return false;
 }
+
+// fix white moving back down
+// fix pawn cant go to a place where a pawn already was 
